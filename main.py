20775c3af44ae29ca83129de13e16e1c55192190
gleb <gleb.demin.04@mail.ru>
2022-10-08 22:40:35 +0300

    add news route

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import aiohttp
+import async_timeout
+import feedparser
 from fastapi import FastAPI
 
 app = FastAPI()
+RSS_URL = "https://www.vedomosti.ru/rss/rubric/"
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def fetch(session, url):
+    with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            return await response.text()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/news/{theme}")
+async def theme_news(theme: str):
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, f"{RSS_URL}{theme}")
+        rss = feedparser.parse(html)
+        print(rss)
+        return rss.entries
